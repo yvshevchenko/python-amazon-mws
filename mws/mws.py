34543252658e32d92a3e3577837aaedc2fa39e7a
yvshevchenko <yvshevchenko@gmail.com>
2016-05-14 10:51:31 +0300

    adde financial to _all_

--- mws/mws.py
+++ mws/mws.py
@@ -32,6 +32,7 @@ __all__ = [
     'Products',
     'Recommendations',
     'Sellers',
+    'Finances'
 ]
 
 # See https://images-na.ssl-images-amazon.com/images/G/01/mwsportal/doc/en_US/bde/MWSDeveloperGuide._V357736853_.pdf page 8
